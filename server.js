5bb62675621bf94ad424c669b20853d433104bde
Jesus_0414 <jesus140400@gmail.com>
2020-05-12 17:38:41 -0700

    json

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000
 
 app.use('/src', express.static('src'));
 app.use('/public', express.static('public'));
@@ -72,4 +72,4 @@ app.get('/vertex-color', (req, res)=>{
     res.sendFile(`${__dirname}/public/vertex-color.htm`);
 });
 
-app.listen(port, console.log(`listening at http://localhost:${port}`))
+app.listen(port, console.log(`listening at http://localhost:${port}`));
